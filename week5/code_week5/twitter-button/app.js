8ba443a0e823a5df52a0ef722daf53479bc5ddb9
Francis Li <mail@francisli.com>
2017-10-18 14:12:56 -0700

    Updated twitter button example

--- week5/code_week5/twitter-button/app.js
+++ week5/code_week5/twitter-button/app.js
@@ -1,37 +1,38 @@
 //Import Twitter and J5 modules
 var Twitter = require('twitter')
 var five = require("johnny-five");
-var temperature;
+
+// Load secret settings from .env file
+require('dotenv').config();
 
 // Authenticates with Twitter using the (unofficial) twitter
 // package on npm. This is required for using the interesting parts
 // of the API, e.g. streaming or posting tweets.
 var client = new Twitter({
-    consumer_key: 'H7WE4DmlhgyqhJtJXKVVeSCxd',
-    consumer_secret: '5v5e1RrQOspeUzLm7XqoLny8PPoPCnTakIgR2Qc3K1JHURNCWn',
-    access_token_key: '3035181914-VdvXFCCouAD5BN7brkmMnv95U842QhljYTSj9x6',
-    access_token_secret: '4XtCCHgqnDu6WWJ7aC0qjV24xDbzhAaEYvI6ZjUuqZwgv'
-})
-
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
 //nice date format function
 function getDate() {
-    var date = new Date();
-    var month = date.getMonth() + 1;
-    var day = date.getDate();
-    var hour = date.getHours();
-    var min = date.getMinutes();
-    var sec = date.getSeconds();
+  var date = new Date();
+  var month = date.getMonth() + 1;
+  var day = date.getDate();
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
 
-    month = (month < 10 ? "0" : "") + month;
-    day = (day < 10 ? "0" : "") + day;
-    hour = (hour < 10 ? "0" : "") + hour;
-    min = (min < 10 ? "0" : "") + min;
-    sec = (sec < 10 ? "0" : "") + sec;
+  month = (month < 10 ? "0" : "") + month;
+  day = (day < 10 ? "0" : "") + day;
+  hour = (hour < 10 ? "0" : "") + hour;
+  min = (min < 10 ? "0" : "") + min;
+  sec = (sec < 10 ? "0" : "") + sec;
 
-    var str = date.getFullYear() + "-" + month + "-" + day + "_" + hour + ":" + min + ":" + sec;
+  var str = date.getFullYear() + "-" + month + "-" + day + "_" + hour + ":" + min + ":" + sec;
 
-    return str;
+  return str;
 }
 
 //set up a new j5 board
@@ -39,22 +40,17 @@ var board = new five.Board();
 
 
 board.on("ready", function() {
-    temperature = new five.Temperature({
-        controller: "TMP36",
-        pin: "A0"
-    });
+  var button = new five.Button({
+    pin: 2,
+  });
 
-    var button = new five.Button({
-        pin: 2,
+  button.on("press", function() {
+    var time = Date.now();
+    client.post('statuses/update', {
+      status: 'Hello Internet! ' + getDate()
+    }, function(err, tweets) {
+      if (err) throw new Error(err[0].message)
+      console.log('Tweeted successfully!')
     });
-
-    button.on("press", function() {
-        var time = Date.now();
-        client.post('statuses/update', {
-            status: 'Hello Internet! ' + getDate()
-        }, function(err, tweets) {
-            if (err) throw new Error(err[0].message)
-            console.log('Tweeted successfully!')
-        });
-    });
+  });
 });
