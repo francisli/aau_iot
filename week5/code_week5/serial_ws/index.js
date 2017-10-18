3d98ac2998c5868bdecd10fe6ef097700ab01923
Francis Li <mail@francisli.com>
2017-10-18 13:12:04 -0700

    Update serial_ws with updated serialport syntax, fix web script so works with data as formatted from AnalogInOutSerial.

--- week5/code_week5/serial_ws/index.js
+++ week5/code_week5/serial_ws/index.js
@@ -1,12 +1,12 @@
-var com = require("serialport");
+var SerialPort = require("serialport");
 var app = require('express')();
 
 var server = app.listen(3000);
 var io = require('socket.io')(server);
 
-var serialPort = new com.SerialPort("/dev/cu.usbmodem1411", {
+var serialPort = new SerialPort("/dev/cu.usbmodem14111", {
     baudrate: 9600,
-    parser: com.parsers.readline('\r\n')
+    parser: SerialPort.parsers.readline('\r\n')
 });
 
 //Serve index.html when some make a request of the server
@@ -22,5 +22,3 @@ serialPort.on('data', function(data) {
     io.sockets.emit('data',data);
     console.log(data);
 });
-
-
